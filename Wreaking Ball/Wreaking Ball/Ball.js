04d6f34f8fc1c720c83bb92bb1f2837b032e314c
nayaKudssi <76819985+nayaKudssi@users.noreply.github.com>
2021-03-17 10:42:09 -0500

    Add files via upload

---
+++ Wreaking Ball/Wreaking Ball/Ball.js
@@ -0,0 +1,25 @@
+class Ball
+{
+    constructor(x, y,radius) 
+    {
+        var options = 
+        {
+            'frictionAir':0.005,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        World.add(world, this.body);
+      }
+      display()
+      {
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        fill("red");
+        ellipse(0, 0, this.radius,this.radius);
+        pop();
+      }
+}
